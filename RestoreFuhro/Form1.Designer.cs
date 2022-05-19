1ef0a2475500adc2341f432ca727e20fe65bc76c
Nathaly Moura <nathalyfmoura@msn.com>
2022-05-19 20:34:01 -0300

    Add project files.

---
+++ RestoreFuhro/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace RestoreFuhro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Play = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Play
+            // 
+            this.Play.Location = new System.Drawing.Point(53, 49);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(83, 66);
+            this.Play.TabIndex = 0;
+            this.Play.Text = "Play";
+            this.Play.UseVisualStyleBackColor = true;
+            this.Play.Click += new System.EventHandler(this.Play_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Play);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Play;
+    }
+}
+
---
+++ RestoreFuhro/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RestoreFuhro
+{
+
+    public partial class Form1 : Form
+    {
+            public Form1()
+            {
+                InitializeComponent();
+            }
+
+            private void Play_Click(object sender, EventArgs e)
+            {
+            var data = File.ReadAllText("C:\\pablo\\texto.txt");
+            MessageBox.Show(data);
+
+        }
+        }
+    }
+
